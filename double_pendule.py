87cdd9417cddf7daa4c5f619381bb11563444eb9
ILYASSE07-v <ilyassepr@gmail.com>
2026-04-24 14:25:15 +0100

    fixed the pendulum now it swings could add more fonctionality

--- double_pendule.py
+++ double_pendule.py
@@ -18,24 +18,40 @@ h.penup()
 h.pencolor("green")
 h.pensize(5)
 
-#variable
+# variable
 length_1 = 300
 length_2 = 200
 grav_const = 1
 mass1 = 1
 mass2 = 1
+phi1_d = 0
+phi2_d = 0
+temp1 = 100
+temp2 = 20
 
-def step(phi1, phi2):
-	phi1_d = 0
-	phi2_d = 0
-    phi1_dd = (-grav_const * (2 *mass1 + mass2)* m.sin(phi1) - mass2 * grav_const * m.sin(phi1 - 2 * phi2) - 2 * m.sin(phi1 - phi2) * mass2 * (((phi2_d) ** 2) * length_2 + ((phi1_d) ** 2) * length_1 * m.cos(phi1 - phi2)))/ (length_1 * (2 * mass1 + mass2 - mass2 * m.cos(2 * phi1 - 2 * phi2)))
-	phi2_dd = (2 * m.sin(phi1 - phi2) * (((phi1_d) ** 2) * length_1 * (mass1 + mass2) + grav_const * (mass1 + mass2) * m.cos(phi1) + ((phi2_d) ** 2) * length_2 * mass2 * m.cos(phi1 - phi2))) / (length_2 * (2 * mass1 + mass2 - mass2 * m.cos(2 * phi1 - 2 * phi2)))
+
+def step(tet1, tet2):
+    global phi1_d, phi2_d, temp1, temp2
+    dt = 0.05
+    dev = (2 * mass1 + mass2 - mass2 * m.cos(2 * tet1 - 2 * tet2))
+    phi1_dd = (-grav_const * (2 * mass1 + mass2) * m.sin(tet1) - mass2 * grav_const * m.sin(tet1 - 2 * tet2) - 2 * m.sin(tet1 -
+               tet2) * mass2 * (((phi2_d) ** 2) * length_2 + ((phi1_d) ** 2) * length_1 * m.cos(tet1 - tet2))) / (length_1 * dev)
+    phi2_dd = (2 * m.sin(tet1 - tet2) * (((phi1_d) ** 2) * length_1 * (mass1 + mass2) + grav_const * (mass1 +
+               mass2) * m.cos(tet1) + ((phi2_d) ** 2) * length_2 * mass2 * m.cos(tet1 - tet2))) / (length_2 * dev)
+    phi1_d += phi1_dd * dt
+    phi2_d += phi2_dd * dt
+    phi1_d *= 0.9999999
+    phi2_d * + 0.9999999
+    tet1 += phi1_d * dt
+    tet2 += phi2_d * dt
+    temp1 = tet1
+    temp2 = tet2
+    return tet1, tet2
+
+
 def draw(phi1, phi2):
     h.clear()
     h.pendown()
-
-    phi1 = m.radians(phi1)
-    phi2 = m.radians(phi2)
 
     x = length_1 * m.sin(phi1)
     y = 300 - length_1 * m.cos(phi1)
@@ -52,7 +68,8 @@ def draw(phi1, phi2):
     h.goto(0, 300)
 
 
-draw(0, 0)
-
-screen.update()
-turtle.exitonclick()
+temp1 = m.radians(temp1)
+temp2 = m.radians(temp2)
+while True:
+    draw(*step(temp1, temp2))
+    screen.update()
